828aa9d10d53507e772a55c30f2317350e6c970b
Haima-santhosh <hymasanthosh14@gmail.com>
2025-06-30 18:12:23 +0530

    Changes Added

---
+++ new-app/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+
+
+function App() {
+  
+const [count, setCount]=useState(0)
+const addCount=()=>
+{
+  setCount(count+1)
+}
+const subCount=()=>{
+  setCount(count-1)
+}
+  return (
+    <>
+      <h2>{count}</h2>
+      <button onClick={addCount}>ADD</button>
+      <button onClick={subCount}>SUB</button>
+      
+      
+    </>
+  )
+}
+
+export default App
