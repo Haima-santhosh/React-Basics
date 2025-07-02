bbd32c55a4150a66c4507f8eacc84dfb20610803
Haima-santhosh <hymasanthosh14@gmail.com>
2025-07-02 22:36:13 +0530

    New branch Added

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,36 +1,74 @@
-import { Children, useState } from 'react'
+import { useState } from "react"
+import "./assets/css/App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const[products,setProducts]=useState([
+
+{
+ title:"Headset-1" ,
+ image:"https://hk-media.apjonlinecdn.com/catalog/product/5/3/53L34AA_UUF-1_T1678886510.png",
+ price:"1500/-"
+},
+
+{
+ title:"Headset-2" ,
+ image:"https://hk-media.apjonlinecdn.com/catalog/product/5/3/53L34AA_UUF-1_T1678886510.png",
+ price:"2000/-"
+},
+
+{
+ title:"Headset-3" ,
+ image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjHWYvFuVHE5IG09eyoNGE7S3lXsb5CEGyJw&s",
+ price:"1000/-"
+},
+
+{
+ title:"Headset-4" ,
+ image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2lybS1fYZpyU1gnbQiooeekMtsJkWEbGFQQ&s",
+ price:"2500/-"
+}
+
+
+
+
+
+
+  ]
+
+  )
+  
 
   return (
     <>
       
-      <h1>Hello World!</h1>
-      <div className="card">
-         <button handleClick={() => setCount((count) => count + 1)}>
-          New Count is {count}
-        </button>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <h1>Products</h1>
+      <div className="productsContainer">
+        {products.map((products,index)=>{
+          return(
+
+              <div className="productItem">
+     
+        <img src={products.image} alt={products.title} className="productImage"  />
+        <p className="productTitle">{products.title}</p>
+        <p className="productPrice">Price : {products.price}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+   )
+        })}
+      
+
+
+      </div>
+
+
+       
+
+      
     </>
   )
 }
 
 export default App
 
-const Button=({children,handleClick})=>{
-  return(
-    <button onClick={(()=>handleClick((count) => count + 1))}>
-    {children}
-    </button>
-  )
-}
+
