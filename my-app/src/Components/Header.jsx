b454910756667dde722823e6b2fd59f0ff476fb4
Haima-santhosh <hymasanthosh14@gmail.com>
2025-07-08 13:20:38 +0530

    Updated File

--- my-app/src/Components/Header.jsx
+++ my-app/src/Components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const NavLink = 
+const NavLink =
     [
         {
             url: "",
@@ -18,7 +18,7 @@ const NavLink =
 
         },
         {
- url: "",
+            url: "",
             text: "Special"
 
         }
@@ -29,16 +29,30 @@ const Header = () => {
     return (
         <>
             <header className='grid md:grid-cols-2 sm:grid-cols-[20%_80%]  lg:grid-cols-2 xl:grid-cols-2 gap-5 w-full h-20 border b-2 shadow-xl'>
-                <div className="h-full flex justify-center items-center">
-                    <h2 className='text-center text-2xl uppercase font-extrabold text-red-800 italic'>
-                        <img className='w-20' src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=food&sf=&txt_keyword=All" alt="" /></h2>
+                <div className="h-full flex justify-around items-center">
 
+                    <img className='w-16 ml-10' src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?
+                    nwm=1&nws=1&industry=food&sf=&txt_keyword=All" alt="" />
+                    <h2 className='text-center mx-auto w-fit  text-2xl uppercase font-extrabold text-red-800 italic relative'>SWIGGY
+                    <span className='text-red-600 capitalize font-extrabold absolute text-[8px] -bottom-4 -left-0'>everything is delicious</span></h2>
                 </div>
-                <div className="text-center h-full flex justify-around items-center">
-                    {NavLink.map((item)=>(
 
-                        <a  key={item.text} href={item.url} className=' text-red-400 hover:text-red-700'>{item.text}</a>
-                  
+
+                {/* <div className=' h-full  flex justify-around items-center  '>
+                <img className='w-16 ml-10 ' src="https://static.vecteezy.com/system/resources/previews/005/513/590/non_2x/
+                catering-quality-food-design-premium-logo-vector.jpg" alt="" />
+                <h2 className='text-center  relative mx-auto w-fit text-2xl uppercase font-extrabold italic text-red-500 border'>
+                    swiggy!
+                    <span className='text-red-700 absolute text-[8px] -bottom-4 -left-10   '>everything is delicious</span>
+                </h2>
+            </div> */}
+
+
+                <div className="text-center h-full flex justify-around items-center">
+                    {NavLink.map((item) => (
+
+                        <a key={item.text} href={item.url} className=' text-red-400 hover:text-red-700'>{item.text}</a>
+
                     ))}
                 </div>
             </header>
